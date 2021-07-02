d7bf10b806ead010a9c5b06e548d97aa6d781c23
k.matsui <matsui.new@gmail.com>
2021-07-02 14:31:32 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+    
+    include SessionsHelper
     include Pagy::Backend
+    
+    private
+    
+    def require_user_logged_in
+        unless logged_in?
+            redirect_to login_url
+        end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :destroy]
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
